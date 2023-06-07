bdc9c113c2f503bd9615692e92bd4f509e4e5530
masqolani <m.asqolani@gmail.com>
2023-06-07 09:34:28 +0700

    add store transaction db

---
+++ db/sqlc/store_test.go
@@ -0,0 +1,75 @@
+package db
+
+import (
+	"context"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestStore_TransferTx(t *testing.T) {
+	store := NewStore(testDB)
+
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+
+	// run n concurrent transfer transactions for the best testing db transaction
+	n := 5
+	amount := int64(10000)
+
+	// make a channel and goroutine function
+	errors := make(chan error)
+	results := make(chan TransferTxResult)
+
+	for i := 0; i < n; i++ {
+		go func() {
+			result, err := store.TransferTx(context.Background(), TransferTxParams{
+				FromAccountID: account1.ID,
+				ToAccountID:   account2.ID,
+				Amount:        amount,
+			})
+
+			errors <- err
+			results <- result
+		}()
+	}
+
+	// check results
+	for i := 0; i < n; i++ {
+		err := <-errors
+		require.NoError(t, err)
+
+		result := <-results
+		require.NotEmpty(t, result)
+
+		// check transfer
+		transfer := result.Transfer
+		require.NotEmpty(t, transfer)
+		require.Equal(t, account1.ID, transfer.FromAccountID)
+		require.Equal(t, account2.ID, transfer.ToAccountID)
+		require.Equal(t, amount, amount)
+
+		getTransfer, err := store.GetTransfer(context.Background(), transfer.ID)
+		require.NoError(t, err)
+		require.NotEmpty(t, getTransfer)
+
+		// check from entry
+		fromEntry := result.FromEntry
+		require.NotEmpty(t, fromEntry)
+		require.Equal(t, account1.ID, fromEntry.AccountID)
+		require.Equal(t, -amount, fromEntry.Amount)
+
+		getFromEntry, err := store.GetEntry(context.Background(), fromEntry.ID)
+		require.NoError(t, err)
+		require.NotEmpty(t, getFromEntry)
+
+		// check to entry
+		toEntry := result.ToEntry
+		require.NotEmpty(t, toEntry)
+		require.Equal(t, account2.ID, toEntry.AccountID)
+		require.Equal(t, amount, toEntry.Amount)
+
+		getToEntry, err := store.GetEntry(context.Background(), toEntry.ID)
+		require.NoError(t, err)
+		require.NotEmpty(t, getToEntry)
+	}
+}
